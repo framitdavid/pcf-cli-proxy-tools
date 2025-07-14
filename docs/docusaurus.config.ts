8a025438f49169f66b3025bdb43b55d9b1e6a67e
David Ovrelid <david@framit.no>
2025-07-15 00:06:20 +0200

    docs config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,15 +15,13 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://framitdavid.github.io',
+  baseUrl: '/pcf-cli-proxy-tools/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'framitdavid',
+  projectName: 'pcf-cli-proxy-tools',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
